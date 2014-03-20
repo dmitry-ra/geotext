4c92635aeaed2a0ce8b78449da24e21584f9e539
dmitry-ra <dmitry.razumikhin@gmail.com>
2014-03-21 01:31:49 +0400

    Added js

---
+++ javascripts/geoparse.js
@@ -0,0 +1,42 @@
+﻿var GeoText = (function (gt) {
+    function normalize(value) {
+        return value.split(/\s+/).join('').split(/[,\u066B]/).join('.');
+    }
+    function getValued(array) {
+        return array.filter(function(x){return typeof x != 'undefined';});
+    }
+    gt.parse = function(value, parseAltitude, altitudeUnit) {
+        var result = {};
+        var coords = value.match(/(\d+\s*\u00B0.*?[NEWS])/gi);
+        for (var i = 0; i < coords.length; i++) {
+            var coord = normalize(coords[i]);
+            var sign = coord.charAt(coord.length - 1).toUpperCase();
+            var values = getValued(coord.match(/(\d+\u00B0)(\d+\'){0,1}(\d+\.{0,1}\d+"){0,1}/));
+            var d = 0, m = 0, s = 0;
+            for (var j = 1; j < values.length; j++) {
+                var val = values[j];
+                switch (val.charAt(val.length - 1)) {
+                    case '\u00B0': d = parseFloat(val); break;
+                    case "'": m = parseFloat(val); break;
+                    case '"': s = parseFloat(val); break;
+                }
+            }
+            var v = d + (m / 60.0) + (s / 3600.0);
+            switch (sign) {
+                case 'N': result.lat = v; break;
+                case 'S': result.lat = -v; break;
+                case 'E': result.lon = v; break;
+                case 'W': result.lon = -v; break;
+            }
+        }
+        if (parseAltitude) {
+            altitudeUnit = altitudeUnit || 'm';
+            var av = value.match(new RegExp('(\\d+[\\.,\u066B]{0,1}\\d+)\\s*' + altitudeUnit));
+            if (av && av.length > 1) {
+                result.alt = parseFloat(normalize(av[1]));
+            }
+        }
+        return result;
+    };
+    return gt;
+}(GeoText || {}));
---
+++ javascripts/geoprint.js
@@ -0,0 +1,41 @@
+﻿var GeoText = (function (gt) {
+    var degreeSymbol = '\u00B0';
+    var localZero = (0).toLocaleString();
+    var localDecimalSeparator = (1.1).toLocaleString().charAt(1);
+    function zeroPad(value) {
+        var string = value.toLocaleString();
+        return (value < 10) ? (localZero + string) : string;
+    }
+    function toFixed(number, precision) {
+        var multiplier = Math.pow(10, precision);
+        return Math.round(number * multiplier) / multiplier;
+    }
+    gt.deg2str = function(value, isLatitude) {
+        var sign = isLatitude ? (value >= 0 ? 'N' : 'S') : (value >= 0 ? 'E' : 'W');
+        value = Math.abs(value);
+        var frac = value % 1;
+        var deg = (value | 0).toLocaleString();
+        var min = zeroPad(frac * 60 | 0);
+        var sec = zeroPad(toFixed(frac * 3600 % 60, 3));
+        return deg.concat(degreeSymbol, min, "'", sec, '"', sign);
+    }
+    gt.lat2str = function(latitude) {
+        return gt.deg2str(latitude, true);
+    }
+    gt.lon2str = function(longitude) {
+        return gt.deg2str(longitude, false);
+    }
+    gt.point2str = function(latitude, longitude, altitude, altitudeUnit) {
+        var result = [];
+        if (typeof latitude != 'undefined')
+            result.push(gt.lat2str(latitude));
+        if (typeof longitude != 'undefined')
+            result.push(gt.lon2str(longitude));
+        if (typeof altitude != 'undefined') {
+            var unit = altitudeUnit || 'm';
+            result.push(altitude.toLocaleString() + unit);
+        }
+        return result.join(' ');
+    }
+    return gt;
+}(GeoText || {}));
